85ad8aef1050fa304b0fe7a3459018dcf4403d7c
Shai <shai.bianchi@mercadolibre.com>
2017-01-10 20:37:45 -0300

    Rename KernelSlider -> KernelSumSlider

--- src/KernelSlider.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/KernelSlider.h"
-
-template <class PixelType>
-KernelSlider<PixelType>::KernelSlider(const Mat& image, const Kernel& kernel)
-    : image(image), kernel(kernel) {}
-
