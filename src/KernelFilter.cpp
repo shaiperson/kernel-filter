798cf420ff9249e5a8a0c1156e114367f77101f7
Shai <shai.bianchi@mercadolibre.com>
2017-01-10 20:39:24 -0300

    Implement KernelFilter::convolve

--- src/KernelFilter.cpp
+++ src/KernelFilter.cpp
@@ -9,3 +9,23 @@ Mat KernelFilter<PixelType, KernelType>::compute() const {
     return convolve(imageToConvolutionData());
 }
 
+template <class PixelType, class KernelType>
+Mat KernelFilter<PixelType, KernelType>::convolve(const Mat& data) const {
+    KernelSumSlider<PixelType, KernelType> slider(data, kernel);
+    Mat result(resultRows, resultCols, image.type());
+    Ptr<PixelType> resultPtr = result.ptr(0);
+
+    size_t resultRows = data.rows - (kernel.weights).rows + 1;
+    size_t resultCols = data.cols - (kernel.weights).cols + 1;
+
+    for (size_t i = (kernel.pin).y; i < (kernel.pin).y + resultRows; ++i) {
+        for (size_t j = (kernel.pin).x; j < (kernel.pin).x + resultCols; ++j) {
+            Scalar channelMeans = slider.sumOnTarget(Point(i,j));
+            for (size_t c = 0; c < data.channels(); ++c) {
+                *resultPtr++ = channelMeans[c] / (kernel.weights).total();
+            }
+        }
+    }
+    
+    return result;
+}
