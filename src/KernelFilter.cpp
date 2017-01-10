b3acc6c6983d39a276c83de49e9132a93735b5ff
Shai <shai.bianchi@mercadolibre.com>
2017-01-10 20:39:01 -0300

    KernelFilter implementation refactor

--- src/KernelFilter.cpp
+++ src/KernelFilter.cpp
@@ -1,11 +1,11 @@
 #include "../include/KernelFilter.h"
 
-template <class T>
-KernelFilter<T>::KernelFilter(const Mat& image, const Kernel& kernel)
+template <class PixelType, class KernelType>
+KernelFilter<PixelType, KernelType>::KernelFilter(const Mat& image, const Kernel& kernel)
     : image(image), kernel(kernel) {}
 
-template <class T>
-Mat KernelFilter<T>::compute() const {
+template <class PixelType, class KernelType>
+Mat KernelFilter<PixelType, KernelType>::compute() const {
     return convolve(imageToConvolutionData());
 }
 
