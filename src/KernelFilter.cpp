0641a2732c90e1fd8cfbb0b24dcf30853a885068
Shai <shai.bianchi@mercadolibre.com>
2017-01-10 01:59:53 -0300

    Implement KernelFilter compute method

--- src/KernelFilter.cpp
+++ src/KernelFilter.cpp
@@ -4,3 +4,8 @@ template <class T>
 KernelFilter<T>::KernelFilter(const Mat& image, const Kernel& kernel)
     : image(image), kernel(kernel) {}
 
+template <class T>
+Mat KernelFilter<T>::compute() const {
+    return convolve(imageToConvolutionData());
+}
+
