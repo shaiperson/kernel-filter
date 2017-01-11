ea4f22e43dfbc6a2f9c50b623d3b138ce609edc5
Shai <shai.bianchi@mercadolibre.com>
2017-01-11 20:57:24 -0300

    Refactor, remove type Kernel

--- include/KernelFilter.h
+++ include/KernelFilter.h
@@ -2,21 +2,30 @@
 #define KERNELFILTER_H
 
 #include "header.h"
-#include "Kernel.h"
 
 class KernelFilter {
 
 public:
-    KernelFilter(const Mat& image, const Kernel& kernel);
+    KernelFilter(const Mat& data, const Mat& kernel, const Point& kernelPin);
     Mat compute() const;
 
-private:
-    Mat image;
-    Kernel kernel;
+protected:
+    Mat data;
+    Mat kernel;
+    Point kernelPin;
 
-    Mat convolve(const Mat& data) const;
-    Scalar sumOnTarget(const Point& target, const Mat& data) const;
-    virtual Mat imageToConvolutionData() const = 0;
+    virtual Mat toConvolutionData() const = 0;
+    Mat convolve(const Mat& convolutionData) const;
+    Vec3d convolutionStep(const Mat& data, const Point& target) const;
+
+};
+
+class KernelFilterEdgeCrop : public KernelFilter {
+
+    using KernelFilter::KernelFilter;
+    Mat toConvolutionData() const {
+        return data;
+    }
 
 };
 
--- src/KernelFilter.cpp
+++ src/KernelFilter.cpp
@@ -1,40 +1,37 @@
 #include "../include/KernelFilter.h"
 
-KernelFilter::KernelFilter(const Mat& image, const Kernel& kernel)
-    : image(image), kernel(kernel) {}
+KernelFilter::KernelFilter(const Mat& data, const Mat& kernel, const Point& kernelPin)
+    : data(data), kernel(kernel), kernelPin(kernelPin) {}
 
 Mat KernelFilter::compute() const {
-    return convolve(imageToConvolutionData());
+    return convolve(toConvolutionData());
 }
 
-Scalar KernelFilter::sumOnTarget(const Point& target, const Mat& data) const {
-    Scalar sum(0,0,0);
-    size_t blockFirstRow = target.y - (kernel.pin).y;
-    size_t blockFirstCol = target.x - (kernel.pin).x;
-    for (size_t i = 0; i < (kernel.weights).rows; ++i)
-        for (size_t j = 0 ; j < (kernel.weights).cols; ++j)
+Vec3d KernelFilter::convolutionStep(const Mat& data, const Point& target) const {
+    Vec3d sum = {0,0,0};
+    size_t blockFirstRow = target.y - kernelPin.y;
+    size_t blockFirstCol = target.x - kernelPin.x;
+    for (size_t i = 0; i < kernel.rows; ++i)
+        for (size_t j = 0; j < kernel.cols; ++j)
             for (size_t c = 0; c < data.channels(); ++c)
                 sum[c] +=
                     data.at<Vec3d>(blockFirstRow + i, blockFirstCol + j)[c] *
-                    (kernel.weights).at<double>(i,j);
+                    kernel.at<double>(i,j);
+
     return sum;
 }
 
 Mat KernelFilter::convolve(const Mat& data) const {
-    size_t resultRows = data.rows - (kernel.weights).rows + 1;
-    size_t resultCols = data.cols - (kernel.weights).cols + 1;
+    size_t resultRows = data.rows - kernel.rows + 1;
+    size_t resultCols = data.cols - kernel.cols + 1;
+    Mat result(resultRows, resultCols, CV_64FC3);
 
-    Mat result(resultRows, resultCols, image.type());
-    Ptr<Vec3d> resultPtr = result.ptr(0);
+    int pinY = kernelPin.y;
+    int pinX = kernelPin.y;
 
-    for (size_t i = (kernel.pin).y; i < (kernel.pin).y + resultRows; ++i) {
-        for (size_t j = (kernel.pin).x; j < (kernel.pin).x + resultCols; ++j) {
-            Scalar channelMeans = sumOnTarget(Point(i,j), data);
-            for (size_t c = 0; c < data.channels(); ++c) {
-                *resultPtr++ = channelMeans[c] / (kernel.weights).total();
-            }
-        }
-    }
+    for (size_t i = 0; i < resultRows; ++i)
+        for (size_t j = 0; j < resultCols; ++j)
+            result.at<Vec3d>(i,j) = convolutionStep(data, Point(pinY+i, pinX+j));
 
     return result;
 }
